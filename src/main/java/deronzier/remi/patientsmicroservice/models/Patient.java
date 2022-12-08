e5f5e0eb18cf1aff8e1526d247a7d0ed31079207
remax21 <deronzier.remi@gmail.com>
2022-12-09 00:45:40 +0100

    :sparkles: (Patient Controller) Create patient controller

--- src/main/java/deronzier/remi/patientsmicroservice/models/Patient.java
+++ src/main/java/deronzier/remi/patientsmicroservice/models/Patient.java
@@ -4,11 +4,14 @@ import java.time.LocalDate;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
 import lombok.Data;
 
 @Entity
@@ -19,27 +22,28 @@ public class Patient {
     private long id;
 
     @Column(nullable = false)
-    @NotBlank(message = "Patient last name cannot be null")
+    @NotBlank(message = "Patient last name cannot be null", groups = CreateClass.class)
     private String lastName;
 
     @Column(nullable = false)
-    @NotBlank(message = "Patient last name cannot be null")
+    @NotBlank(message = "Patient last name cannot be null", groups = CreateClass.class)
     private String firstName;
 
     @Column(nullable = false)
-    @NotBlank(message = "Patient date of birth cannot be null")
+    @NotNull(groups = CreateClass.class)
+    @Past
     private LocalDate dateOfBirth;
 
     @Column(nullable = false)
-    @NotBlank(message = "Patient sex cannot be null")
-    @Enumerated
+    @NotNull(groups = CreateClass.class)
+    @Enumerated(EnumType.STRING)
     private Sex sex;
 
     @Column(nullable = false)
-    @NotBlank(message = "Patient address cannot be null")
+    @NotBlank(message = "Patient address cannot be null", groups = CreateClass.class)
     private String address;
 
     @Column(nullable = false)
-    @NotBlank(message = "Patient phone number cannot be null")
+    @NotBlank(message = "Patient phone number cannot be null", groups = CreateClass.class)
     private String phoneNumber;
 }
---
+++ src/main/java/deronzier/remi/patientsmicroservice/models/UpdateClass.java
@@ -0,0 +1,5 @@
+package deronzier.remi.patientsmicroservice.models;
+
+public interface UpdateClass {
+
+}
