cb150352eadc71ab2f5bbb548643364b229a2b05
remax21 <deronzier.remi@gmail.com>
2022-12-08 22:41:50 +0100

    :tada: (All documents) First commit

---
+++ src/main/java/deronzier/remi/patientsmicroservice/PatientsMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package deronzier.remi.patientsmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientsMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientsMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/deronzier/remi/patientsmicroservice/PatientsMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package deronzier.remi.patientsmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientsMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
