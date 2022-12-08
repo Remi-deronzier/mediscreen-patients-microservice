c90e15114e696cab4db16e6d932bf902cfc389c9
remax21 <deronzier.remi@gmail.com>
2022-12-08 22:43:49 +0100

    :sparkles: (WelcomeController) Create controller

---
+++ src/main/java/deronzier/remi/patientsmicroservice/controllers/WelcomeController.java
@@ -0,0 +1,25 @@
+package deronzier.remi.patientsmicroservice.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * This class is the controller class to welcome a user at /
+ * 
+ * @author Rémi Deronzier
+ */
+@RestController
+public class WelcomeController {
+
+    @Value("${label.welcome}")
+    private String greetings;
+
+    /**
+     * @return String
+     */
+    @GetMapping
+    public String getWelcomeMessage() {
+        return greetings;
+    }
+}
