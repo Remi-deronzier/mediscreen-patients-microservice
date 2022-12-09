10022cc171373031db86e15ceec000d0d5fbfc2f
remax21 <deronzier.remi@gmail.com>
2022-12-10 00:57:58 +0100

    :pencil: (Doc) Add Swagger 2

--- src/main/java/deronzier/remi/patientsmicroservice/PatientsMicroserviceApplication.java
+++ src/main/java/deronzier/remi/patientsmicroservice/PatientsMicroserviceApplication.java
@@ -3,7 +3,10 @@ package deronzier.remi.patientsmicroservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class PatientsMicroserviceApplication {
 
 	public static void main(String[] args) {
