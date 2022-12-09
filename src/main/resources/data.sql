33b7acfea43413ed98ccc861ad5d222a251587b5
remax21 <deronzier.remi@gmail.com>
2022-12-09 22:03:15 +0100

    :card_file_box: (Initialization script) Loading data on initialization

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+--
+-- Dumping data for table `patient`
+--
+
+INSERT INTO `patient` (`last_name`, `first_name`, `date_of_birth`, `sex`, `address`, `phone_number`) VALUES
+("DERONZIER", "Rémi", "1998-03-23", "M", "rue des Lilas Paris", "0606060606"),
+("DERONZIER", "Thomas", "1996-05-21", "M", "rue des clémentines Paris", "0706060606");
+
