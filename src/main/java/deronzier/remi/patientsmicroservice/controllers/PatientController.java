f29225fd13000cf57fd1b96f4cd03fcb6bb099bd
remax21 <deronzier.remi@gmail.com>
2023-04-09 22:31:51 +0200

    :bug: (CORS policy) Implement Cross origin

--- src/main/java/deronzier/remi/patientsmicroservice/controllers/PatientController.java
+++ src/main/java/deronzier/remi/patientsmicroservice/controllers/PatientController.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.SortDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -29,6 +30,7 @@ import deronzier.remi.patientsmicroservice.services.PatientService;
  */
 @RestController
 @RequestMapping("/patients")
+@CrossOrigin(origins = "*")
 public class PatientController {
 
     @Autowired
